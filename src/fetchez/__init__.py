c28d35bd1c82c2e4571aacf98e1389612dcb039a
Matt Fisher <mfisher87@gmail.com>
2026-02-14 15:40:48 -0700

    Use dynamic versioning
    Ensures the `__version__` variable always matches the released software
    version without a need for manual bumps.
    
    CITATION.cff would need to be manually updated.

--- src/fetchez/__init__.py
+++ src/fetchez/__init__.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.4.2"
 __author__ = "Matthew Love"
 __credits__ = "CIRES"
+
+try:
+    from fetchez._version import __version__
+except ImportError:
+    # Fallback when using the package from source without installing
+    # in editable mode with pip (nobody should do this):
+    # <https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs>
+    import warnings
+
+    warnings.warn(
+        "Importing 'fetchez' outside a proper installation."
+        " It's highly recommended to install the package from a stable release or"
+        " in editable mode.",
+        stacklevel=2,
+    )
+    __version__ = "dev"
 
 # Import everything except the individual modules.
 from . import fred
