e67f0513884d413ea293a63b7cc596f8499fe541
Matthew Love <matthew.love@colorado.edu>
2026-02-20 23:49:04 -0800

    argparse fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ extensions = [
     "sphinx.ext.intersphinx",  # Link to other projects' docs
     "sphinx.ext.viewcode",  # Add links to source code
     "sphinx.ext.githubpages",  # Auto-generate .nojekyll for GH Pages
-    "sphinx.ext.argparse", # Argparse
+    "sphinx.ext",
     "myst_parser",  # Parse Markdown files
     "module_table",  # Generate module catalog table
 ]
