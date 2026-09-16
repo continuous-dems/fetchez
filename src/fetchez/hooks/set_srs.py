9eb20a84c4b6689834ed19cbe06c4151715db056
Matthew Love <matthew.love@colorado.edu>
2026-09-16 11:39:34 -0700

    cleanup srs

--- src/fetchez/hooks/set_srs.py
+++ src/fetchez/hooks/set_srs.py
@@ -14,8 +14,7 @@ Change the default 'src_srs' of an entry.
 import os
 import logging
 from fetchez.hooks import FetchHook
-from fetchez.utils import parse_arg_to_dict
-from fetchez.utils import wgs84_to_utm_zone  # Assuming you moved the helper here
+from fetchez.utils import parse_arg_to_dict, wgs84_to_utm_zone
 
 logger = logging.getLogger(__name__)
 
@@ -71,7 +70,7 @@ class SetSrs(FetchHook):
         for mod, entry in entries:
             keys_to_check = []
 
-            # Populate keys to check (unchanged from original)
+            # Populate keys to check
             if self.match_key:
                 val = entry.get(self.match_key)
                 if val is not None:
@@ -103,14 +102,12 @@ class SetSrs(FetchHook):
 
             # Process the assigned SRS
             if assigned_srs is not None:
-                # 1. Handle auto_utm logic
                 if str(assigned_srs).lower() == "auto_utm":
                     region = getattr(mod, "region", None)
 
                     if region:
                         utm_info = wgs84_to_utm_zone(region)
 
-                        # Warn if crossing zones, as discussed
                         if utm_info.get("crosses_zones"):
                             logger.warning(
                                 f"Region crosses UTM zones (Zones {utm_info['zone_min']} to {utm_info['zone_max']}). "
@@ -123,11 +120,9 @@ class SetSrs(FetchHook):
                         )
                         assigned_srs = "EPSG:4326"
 
-                # 2. Append vertical SRS if specified
                 if self.vert_srs:
                     assigned_srs = f"{assigned_srs}+{self.vert_srs}"
 
-                # 3. Apply changes if needed
                 old_srs = entry.get("src_srs", "unknown")
                 if old_srs != assigned_srs:
                     entry["src_srs"] = assigned_srs
@@ -137,95 +132,3 @@ class SetSrs(FetchHook):
                     )
 
         return entries
-
-
-# #!/usr/bin/env python
-# # -*- coding: utf-8 -*-
-
-# """
-# fetchez.hooks.set_srs
-# ~~~~~~~~~~~~~
-
-# Change the default 'src_srs' of an entry.
-
-# :copyright: (c) 2010-2026 Regents of the University of Colorado
-# :license: MIT, see LICENSE for more details.
-# """
-
-# import os
-# import logging
-# from fetchez.hooks import FetchHook
-# from fetchez.utils import parse_arg_to_dict
-
-# logger = logging.getLogger(__name__)
-
-
-# class SetSrs(FetchHook):
-#     """Overrides the src_srs attribute of pipeline entries based on rules.
-
-#     Useful for mapping generic files to specific parser profiles.
-
-#     Usage:
-#         # Universal override
-#         --hook set_srs:srs='EPSG:4326+5703'
-
-#         # Rule-based override
-#         --hook set_srs:rules=nos_hydro=EPSG:3857/lidar=EPSG:26911
-#     """
-
-#     name = "set-srs"
-#     meta_desc = "Override the src_srs of pipeline entries based on rules."
-#     meta_stage = "manifest"
-#     meta_category = "metadata"
-#     meta_aliases = ["set_srs"]
-
-#     def __init__(self, srs=None, default=None, rules=None, match_key=None, **kwargs):
-#         super().__init__(**kwargs)
-#         self.default = default if default is not None else srs
-#         self.match_key = match_key
-
-#         self.rules = parse_arg_to_dict(rules, cast_type=str)
-#         self.rules = {str(k).lower(): str(v) for k, v in self.rules.items()}
-
-#     def run(self, entries):
-#         for mod, entry in entries:
-#             keys_to_check = []
-
-#             if self.match_key:
-#                 val = entry.get(self.match_key)
-#                 if val is not None:
-#                     keys_to_check.append(str(val).lower())
-#             else:
-#                 if getattr(mod, "name", None):
-#                     keys_to_check.append(str(mod.name).lower())
-#                 if entry.get("data_type"):
-#                     keys_to_check.append(str(entry.get("data_type")).lower())
-#                 dst_fn = entry.get("dst_fn")
-#                 if dst_fn:
-#                     keys_to_check.append(dst_fn.lower())
-#                     _, ext = os.path.splitext(dst_fn)
-#                     if ext:
-#                         keys_to_check.append(ext.lower().lstrip("."))
-
-#             assigned_srs = self.default
-#             match_found = False
-
-#             for key in keys_to_check:
-#                 for rule_key, rule_val in self.rules.items():
-#                     if rule_key in key:
-#                         assigned_srs = rule_val
-#                         match_found = True
-#                         break
-#                 if match_found:
-#                     break
-
-#             if assigned_srs is not None:
-#                 old_srs = entry.get("src_srs", "unknown")
-#                 if old_srs != assigned_srs:
-#                     entry["src_srs"] = assigned_srs
-#                     logger.debug(
-#                         f"Changed src_srs from '{old_srs}' to '{assigned_srs}' "
-#                         f"for {os.path.basename(entry.get('dst_fn', ''))}"
-#                     )
-
-#         return entries
